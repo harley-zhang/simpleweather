b537dd2e1013f4536a5a34a9ac90bc3bc050367f
harley-zhang <harleyzgolego@gmail.com>
2024-03-22 20:33:33 -0400

    starting UI

--- src/App.js
+++ src/App.js
@@ -2,8 +2,35 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function App() {
+
+  //const url = 'https://api.openweathermap.org/data/2.5/weather?q=toronto&appid=33a39eabaa2de86ecbb21094b744394c'
+
   return (
-    <div className="App">
+    <div className="app">
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Toronto</p>
+          </div>
+          <div className='temp'>
+            <h1>50°C</h1>
+          </div>
+          <div className='description'>
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className='bottom'>
+          <div className='feels'>
+            <p>65°C</p>
+          </div>
+          <div className='humidity'>
+            <p>20%</p>
+          </div>
+          <div className='wind'>
+            <p>12 km/h</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
